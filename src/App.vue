12e8082bbed37be310381340b855ddbbb7b88802
ariya arabgol <ariya.arabgol@gmail.com>
2025-02-01 14:47:37 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="continer">
     <h1>xo game</h1>
+    <div class="borad">
+      <div v-for="(cell , index ) in board" :key="index" class="cell" @click="makemove(index)">
+
+        {{ cell }}
+      </div>
+<p class="massage">{{massage}}</p>
+<button class="reset" @click="resetGame">شروع مجدد بازی</button>
+
+
+    </div>
 
 
 
