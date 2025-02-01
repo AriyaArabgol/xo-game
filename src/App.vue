253df7da20987fc502cfdcf65d7b2a85a1c71856
ariya arabgol <ariya.arabgol@gmail.com>
2025-02-01 15:20:44 +0330

    add function checkWinner

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,15 @@ methods:{
       this.massage= ` نوبت بازیکن ${this.currentplayer}`
     }
   },
-  
+  checkWinner(){
+    const winpatterns = [
+      [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    ]
+    return winpatterns.some(patterns =>{
+      const [a,b,c] = patterns
+      return this.board[a] && this.board[a]===this.board[b] && this.board[a] === this.board[c]
+    })
+  }
 }
 
 }
